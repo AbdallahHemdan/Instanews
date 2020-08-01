11c049a5871df0892620de62ebab7f2bb48fc2df
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-01 18:35:46 +0200

    Add Suggestion-Header component

---
+++ src/components/SuggestionHeader/SuggestionHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="suggestions-header">
+    <div class="title">Suggestions For You</div>
+    <a href="/all-suggestions" class="see-all">See All</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SuggestionHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.suggestions-header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+
+  .title {
+    color: #8e8e8e;
+    font-weight: 600;
+    margin-right: 5rem;
+  }
+
+  .see-all {
+    text-decoration: none;
+  }
+}
+</style>
