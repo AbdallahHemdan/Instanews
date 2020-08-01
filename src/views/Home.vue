61a9caf822728ad5af7f7f1550023c545a582a97
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-01 16:24:17 +0200

    Create Home page structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,47 @@
 <template>
-  <h1>Hello, Home</h1>
+  <div class="home-container">
+    <div class="left-content">
+      <newsfeed></newsfeed>
+    </div>
+    <div class="right-content" id="sidebar">
+      <h1>Right</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {},
+  data: function() {
+    return {};
+  },
+  components: {
+    newsfeed: () => import('./../components/Newsfeed/Newsfeed'),
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.left-content {
+  float: left;
+  width: 60%;
+  margin-bottom: 30px;
+}
+
+.right-content {
+  position: fixed;
+  left: 62%;
+  width: 40%;
+  padding-left: 35px;
+}
+
+@media (max-width: 768px) {
+  .left-content {
+    width: 100%;
+  }
+
+  .right-content {
+    display: none;
+  }
+}
+</style>
