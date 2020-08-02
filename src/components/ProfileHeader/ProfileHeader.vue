1af2732b8c1fa221d4bb6d568b2a8462e8e30324
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-02 12:13:10 +0200

    Add profile wrapper

---
+++ src/components/ProfileHeader/ProfileHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post-header-container">
+    <profile-upper-header></profile-upper-header>
+    <profile-stories></profile-stories>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileHeader',
+  components: {
+    'profile-upper-header': () => import('./../ProfileUpperHeader/ProfileUpperHeader'),
+    'profile-stories': () => import('./../ProfileStories/ProfileStories'),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
