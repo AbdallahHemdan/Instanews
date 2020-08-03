31c6502dd25c2be8671764b8530a82039c0797e3
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 08:21:24 +0200

    Register Profile-dropdown, Notification-dropdown

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -34,38 +34,8 @@
           >
             <img :src="feature.imgSrc" :alt="feature.title" />
           </a>
-          <div class="dropdown profile-options">
-            <a
-              class="dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <img src="./../../assets/me.jpg" class="profile-img" alt="profile" />
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="/profile">
-                <img src="./../../assets/svgs/avatar.svg" alt="avatar image" class="drop-icon" />
-                Profile
-              </a>
-              <a class="dropdown-item" href="/profile/saved">
-                <img src="./../../assets/svgs/unsaved.svg" alt="saved icon" class="drop-icon" />
-                Saved
-              </a>
-              <a class="dropdown-item" href="/profile/settings">
-                <img src="./../../assets/svgs/settings.svg" alt="settings icon" class="drop-icon" />
-                Settings
-              </a>
-              <div class="logout">
-                <a class="dropdown-item" href="/">
-                  <img src="./../../assets/svgs/logout.svg" alt="logout icon" class="drop-icon" />
-                  Log Out
-                </a>
-              </div>
-            </div>
-          </div>
+          <notification-dropdown></notification-dropdown>
+          <profile-dropdown></profile-dropdown>
         </div>
       </div>
     </div>
@@ -92,140 +62,17 @@ export default {
           title: 'Chat',
           imgSrc: 'https://img.icons8.com/ios/30/000000/sent.png',
         },
-        {
-          href: '/liked',
-          title: 'Liked posts',
-          imgSrc: 'https://img.icons8.com/ios/30/000000/like.png',
-        },
       ],
     };
+  },
+  components: {
+    'notification-dropdown': () => import('./../NotificationsDropdown/NotificationsDropdown'),
+    'profile-dropdown': () => import('./../ProfileDropdown/ProfileDropdown'),
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import './../../style/settings.scss';
-
-.insta-nav {
-  border-bottom: 1px solid #dbdbdb;
-}
-
-.brand-name {
-  font-size: $brand-fz;
-  color: $main-color;
-  font-family: 'Pacifico', cursive;
-
-  &:hover {
-    color: $main-color;
-  }
-
-  &:active {
-    color: #666;
-  }
-}
-
-.icon {
-  margin: 0 10px;
-}
-
-.options-list {
-  display: flex;
-  @media (max-width: 992px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-  }
-}
-
-.search {
-  background-color: rgba(var(--b3f, 250, 250, 250), 1);
-  border: solid 1px rgba(var(--b6a, 219, 219, 219), 1);
-  padding: 5px;
-  border-radius: 3px;
-  width: 215px;
-  color: rgba(var(--f52, 142, 142, 142), 1);
-  @media (max-width: 992px) {
-    width: 100%;
-  }
-
-  &::-webkit-input-placeholder {
-    text-align: center;
-    font-size: 14px;
-  }
-
-  &:-moz-placeholder {
-    text-align: center;
-    font-size: 14px;
-  }
-
-  &::-moz-placeholder {
-    text-align: center;
-    font-size: 14px;
-  }
-
-  &:-ms-input-placeholder {
-    text-align: center;
-    font-size: 14px;
-  }
-
-  &:focus {
-    outline: none;
-
-    &::-webkit-input-placeholder {
-      text-align: left;
-    }
-
-    &:-moz-placeholder {
-      text-align: left;
-    }
-
-    &::-moz-placeholder {
-      text-align: left;
-    }
-
-    &:-ms-input-placeholder {
-      text-align: left;
-    }
-  }
-}
-
-.profile-img {
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-}
-
-.profile-options {
-  margin-left: 10px;
-}
-
-.dropdown-menu {
-  min-width: 14rem;
-  padding: 0.5rem 0 0;
-  margin: 0.5rem 0 0;
-}
-
-.drop-icon {
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  margin-right: 0.5rem;
-}
-
-.dropdown-item {
-  padding: 0.5rem 1rem;
-  display: flex;
-  align-items: center;
-}
-
-.logout {
-  display: flex;
-  border-top: 1px solid #dbdbdb;
-  & > a {
-    justify-content: flex-start;
-    align-items: center;
-  }
-}
+@import './Navbar.scss';
 </style>
