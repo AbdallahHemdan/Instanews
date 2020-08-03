4459152bc3cbd00ff9d7c5bb1792a3d0b64fc278
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 02:14:19 +0200

    Add ProfileGallery wrapper

---
+++ src/components/ProfileGallery/ProfileGallery.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="profile-gallery gallery-container">
+    <div class="gallery" v-if="items">
+      <gallery-item v-for="(item, index) in items" :key="index" :item="item"></gallery-item>
+    </div>
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileGallery',
+  data: function() {
+    return {
+      items: require('./../../mock/Profile/ProfileGallery').default,
+    };
+  },
+  components: {
+    'gallery-item': () => import('./../ProfileGalleryItem/ProfileGalleryItem'),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './ProfileGallery.scss';
+</style>
