b899e90e080e79b12beffaf396ca510c6dc66796
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 06:10:06 +0200

    Add messages servie which contains all mock msgs

---
+++ src/mock/Messages/Messages.js
@@ -0,0 +1,46 @@
+const messages = [
+  {
+    owner: 'abdallah_abusedo',
+    ownerImg: require('./../../assets/Messages/1.jpg'),
+    lastMessage: 'welcome ya welcome !!',
+    lastMessageDate: '1w',
+  },
+  {
+    owner: 'hello_mayuko',
+    ownerImg: require('./../../assets/Messages/2.jpg'),
+    lastMessage: 'Thanks a lot Hemdan',
+    lastMessageDate: '5d',
+  },
+  {
+    owner: 'mark_zuckerberg',
+    ownerImg: require('./../../assets/Messages/3.jpg'),
+    lastMessage: 'I will check it',
+    lastMessageDate: '4w',
+  },
+  {
+    owner: 'nasser_ahmed99',
+    ownerImg: require('./../../assets/Messages/4.jpg'),
+    lastMessage: '7bebe ya sadeky',
+    lastMessageDate: '3d',
+  },
+  {
+    owner: 'sayedalesawy',
+    ownerImg: require('./../../assets/Messages/5.jpg'),
+    lastMessage: 'hhh ...lol :D',
+    lastMessageDate: '2w',
+  },
+  {
+    owner: 'mahmoud.amr.904',
+    ownerImg: require('./../../assets/Messages/6.jpg'),
+    lastMessage: 'soon, isA',
+    lastMessageDate: '2d',
+  },
+  {
+    owner: 'yousssef_walid',
+    ownerImg: require('./../../assets/Messages/7.jpg'),
+    lastMessage: 'isA ya alby',
+    lastMessageDate: '6d',
+  },
+];
+
+export default messages;
