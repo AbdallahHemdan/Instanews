df6b5d1d9f11749961fb6d41ae27a64abe016c69
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 03:14:55 +0200

    Create Explore page

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Hello, Explore</h1>
+  <div class="profile-gallery gallery-container">
+    <div class="gallery" v-if="items">
+      <gallery-item v-for="(item, index) in items" :key="index" :item="item"></gallery-item>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'ProfileGallery',
+  data: function() {
+    return {
+      items: require('./../mock/Explore/Explore').default,
+    };
+  },
+  components: {
+    'gallery-item': () => import('./../components/ProfileGalleryItem/ProfileGalleryItem'),
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './../components/ProfileGallery/ProfileGallery.scss';
+</style>
