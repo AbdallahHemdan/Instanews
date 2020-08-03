b41c182394de5ac6455221e6e466b8ffa4a18297
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 03:16:19 +0200

    Replace content of home-page with mock data

--- src/components/Newsfeed/Newsfeed.vue
+++ src/components/Newsfeed/Newsfeed.vue
@@ -8,72 +8,7 @@ export default {
   name: 'Newsfeed',
   data: function() {
     return {
-      posts: [
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1513836279014-a89f7a76ae86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1588778272105-1ff4b41dbe93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1588783949480-d6ac3a1f9c08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1588786849476-349bcdfe4754?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1537181534458-45dcee76ae90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-        {
-          mainImg:
-            'https://images.unsplash.com/photo-1564769625905-50e93615e769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          creator: 'Hemdan',
-          creatorImg: './../assets/me.jpg',
-          description:
-            'I are going to attempt to kayak the whole length of the Nile in Egypt from Abu Simbel to Ras El Bar in a double kayak. We estimate it will take us around 40 days to kayak those 1500 km. 😍👌',
-        },
-      ],
+      posts: require('./../../mock/Home/Posts').default,
       comments: [
         {
           commenter: 'omar_hemdan',
