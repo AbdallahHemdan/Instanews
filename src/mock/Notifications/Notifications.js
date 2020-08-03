3985ce0647dfc7a6b3b40776606505de2f9d079b
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 08:19:25 +0200

    Create notification service for mock

---
+++ src/mock/Notifications/Notifications.js
@@ -0,0 +1,53 @@
+const notifications = [
+  {
+    owner: 'abdallah_abusedo',
+    ownerImg: require('./../../assets/Messages/1.jpg'),
+    action: 'start following you.',
+    actionDate: '1w',
+    isFollowed: false,
+  },
+  {
+    owner: 'hello_mayuko',
+    ownerImg: require('./../../assets/Messages/2.jpg'),
+    action: 'start following you.',
+    actionDate: '5d',
+    isFollowed: false,
+  },
+  {
+    owner: 'mark_zuckerberg',
+    ownerImg: require('./../../assets/Messages/3.jpg'),
+    action: 'start following you.',
+    actionDate: '4w',
+    isFollowed: false,
+  },
+  {
+    owner: 'nasser_ahmed99',
+    ownerImg: require('./../../assets/Messages/4.jpg'),
+    action: 'start following you.',
+    actionDate: '3d',
+    isFollowed: false,
+  },
+  {
+    owner: 'sayedalesawy',
+    ownerImg: require('./../../assets/Messages/5.jpg'),
+    action: 'start following you.',
+    actionDate: '2w',
+    isFollowed: false,
+  },
+  {
+    owner: 'mahmoud.amr.904',
+    ownerImg: require('./../../assets/Messages/6.jpg'),
+    action: 'start following you.',
+    actionDate: '2d',
+    isFollowed: false,
+  },
+  {
+    owner: 'yousssef_walid',
+    ownerImg: require('./../../assets/Messages/7.jpg'),
+    action: 'start following you.',
+    actionDate: '6d',
+    isFollowed: false,
+  },
+];
+
+export default notifications;
