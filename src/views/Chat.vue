d5ae9a4e0abbc97a841c67350c5865a31523948c
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 05:42:55 +0200

    Create Chat page

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,9 +1,60 @@
 <template>
-  <h1>Hello, Chat</h1>
+  <div class="chat-container">
+    <div class="row">
+      <div class="my-messages">
+        <messages-container></messages-container>
+      </div>
+      <div class="actual-chat">
+        <inbox-container></inbox-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Chat',
+  components: {
+    'messages-container': () => import('./../components/MessagesContainer/MessagesContainer'),
+    'inbox-container': () => import('./../components/InboxContainer/InboxContainer'),
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.row {
+  margin: 0;
+  padding: 0;
+}
+
+.chat-container {
+  background-color: #fff;
+  height: 87vh;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+
+  .my-messages {
+    width: 34%;
+  }
+
+  .actual-chat {
+    width: 66%;
+  }
+
+  @media (max-width: 992px) {
+    .my-messages {
+      width: 20%;
+    }
+
+    .actual-chat {
+      width: 80%;
+    }
+  }
+}
+</style>
