c0765130be93ce11fb6908faa3de06fcf44a39e1
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-03 11:13:56 +0200

    Add orDivider component

---
+++ src/components/OrDivider/OrDivider.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="divider">
+    <strong class="divider-title">or</strong>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.divider {
+  border-top: 1px solid #d9dadc;
+  display: block;
+  line-height: 1px;
+  margin: 25px 0;
+  position: relative;
+  text-align: center;
+}
+
+.divider .divider-title {
+  background: #fff;
+  font-size: 12px;
+  letter-spacing: 1px;
+  padding: 0 15px;
+  text-transform: uppercase;
+  color: #999;
+}
+</style>
