f6586e7ab1c3d4c3f66a498cd4e0f8cfe279516f
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-05 10:18:20 +0200

    Create see all modal for all users suggestions

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,8 +5,33 @@
     <div class="suggestions">
       <div class="suggestions__header">
         <div class="suggestions__title">Suggested</div>
-        <div class="suggestions__seeall">
-          <a href="">See All</a>
+        <div class="suggestions__seeall" data-toggle="modal" data-target="#seeall">
+          See All
+        </div>
+        <div
+          class="modal fade"
+          id="seeall"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal__header">
+                <div class="modal__title">Similar Accounts</div>
+                <div class="modal__close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </div>
+              </div>
+              <div class="modal-body">
+                <follow-item
+                  v-for="(suggestion, index) in suggestions"
+                  :key="index"
+                  :following="suggestion"
+                ></follow-item>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="suggestions__items">
@@ -36,6 +61,7 @@ export default {
     'profile-stories': () => import('./../components/ProfileStories/ProfileStories'),
     'profile-gallery': () => import('./../components/ProfileGallery/ProfileGallery'),
     'user-suggestion': () => import('./../components/UserSuggestionItem/UserSuggestionItem'),
+    'follow-item': () => import('./../components/FollowItem/FollowItem'),
   },
 };
 </script>
@@ -61,6 +87,7 @@ export default {
 
     .suggestions__seeall {
       color: #0095f6;
+      cursor: pointer;
     }
   }
 
@@ -68,5 +95,40 @@ export default {
     display: flex;
     overflow-x: auto;
   }
+}
+
+.modal__header {
+  display: flex;
+  border-bottom: 1px solid #dbdbdb;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  padding: 14px;
+}
+
+.modal-content {
+  border-radius: 12px;
+  width: 400px;
+  height: 400px;
+  margin: auto;
+}
+
+.modal__close {
+  position: absolute;
+  right: 24px;
+  font-size: 2rem;
+  cursor: pointer;
+}
+
+.modal__title {
+  font-weight: 600;
+}
+
+.modal-body {
+  position: relative;
+  -ms-flex: 1 1 auto;
+  flex: 1 1 auto;
+  padding: 1rem;
+  overflow-y: auto;
 }
 </style>
