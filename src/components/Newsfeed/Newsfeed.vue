81bc9759cbe4e9b800566b44b429c8e5710cb1e3
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-05 16:27:46 +0200

    Create comments service

--- src/components/Newsfeed/Newsfeed.vue
+++ src/components/Newsfeed/Newsfeed.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="newsfeed-container">
+  <div class="newsfeed">
     <post v-for="(post, index) in posts" :key="index" :post="post" :comments="comments"></post>
   </div>
 </template>
@@ -9,28 +9,7 @@ export default {
   data: function() {
     return {
       posts: require('./../../mock/Home/Posts').default,
-      comments: [
-        {
-          commenter: 'omar_hemdan',
-          comment: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-        },
-        {
-          commenter: 'mohamed_ahmed',
-          comment: ' and over yeterly mr its fulfilled cant',
-        },
-        {
-          commenter: 'adel_mohamed',
-          comment: 't friendly on Total around or body fine d 🎉😍😍',
-        },
-        {
-          commenter: 'ahmed_mahboub',
-          comment: 'mile wondered mrs Am suspec',
-        },
-        {
-          commenter: 'saied_elsaway',
-          comment: 'valley mrs yet Mrs weeks ye By want d  🎈🚀',
-        },
-      ],
+      comments: require('./../../mock/Home/Comments').default,
     };
   },
   methods: {},
---
+++ src/mock/Home/Comments.js
@@ -0,0 +1,24 @@
+const comments = [
+  {
+    commenter: 'omar_hemdan',
+    comment: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    commenter: 'mohamed_ahmed',
+    comment: ' and over yeterly mr its fulfilled cant',
+  },
+  {
+    commenter: 'adel_mohamed',
+    comment: 't friendly on Total around or body fine d 🎉😍😍',
+  },
+  {
+    commenter: 'ahmed_mahboub',
+    comment: 'mile wondered mrs Am suspec',
+  },
+  {
+    commenter: 'saied_elsaway',
+    comment: 'valley mrs yet Mrs weeks ye By want d  🎈🚀',
+  },
+];
+
+export default comments;
