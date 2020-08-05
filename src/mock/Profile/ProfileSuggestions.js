61b28615a1033566967248bcd0147588c330a220
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-05 07:46:03 +0200

    Add profile suggestion service

---
+++ src/mock/Profile/ProfileSuggestions.js
@@ -0,0 +1,44 @@
+const suggestions = [
+  {
+    img: require('./../../assets/Suggestions/1.jpg'),
+    name: 'jomaoppa',
+    nickName: 'Joma',
+  },
+  {
+    img: require('./../../assets/Suggestions/2.jpg'),
+    name: 'techleadhd',
+    nickName: 'TechLead',
+  },
+  {
+    img: require('./../../assets/Suggestions/3.jpg'),
+    name: 'jarvis',
+    nickName: 'Jarvis Johnson',
+  },
+  {
+    img: require('./../../assets/Suggestions/4.jpg'),
+    name: '_steph.js',
+    nickName: 'Steph',
+  },
+  {
+    img: require('./../../assets/Suggestions/5.jpg'),
+    name: 'morgan.codes',
+    nickName: 'Morgan Richardson',
+  },
+  {
+    img: require('./../../assets/Suggestions/6.jpg'),
+    name: 'joeel56',
+    nickName: 'joeel56 Webdeveloper',
+  },
+  {
+    img: require('./../../assets/Suggestions/7.jpg'),
+    name: 'tiffintech',
+    nickName: 'Tiffany💡',
+  },
+  {
+    img: require('./../../assets/Suggestions/8.jpg'),
+    name: 'mkbhd',
+    nickName: 'Marques Brownlee',
+  },
+];
+
+export default suggestions;
