e5b16eaa9a145c0436f7b712eade4c79561188af
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-08-05 16:27:29 +0200

    Replace user-suggestion assets in home-page

---
+++ src/mock/Home/UserSuggestions.js
@@ -0,0 +1,40 @@
+const userSuggestions = [
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/1.jpg'),
+    username: 'sayedalesawy',
+    suggestionBase: 'popular',
+    id: '1',
+  },
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/2.jpg'),
+    username: 'nasser_ahmed009',
+    suggestionBase: 'popular',
+    id: '2',
+  },
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/3.jpg'),
+    username: 'hellomayuko',
+    suggestionBase: 'popular',
+    id: '3',
+  },
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/4.jpg'),
+    username: 'ranaelkaliouby',
+    suggestionBase: 'New to Instagram',
+    id: '4',
+  },
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/5.jpg'),
+    username: 'amr.sherif.adel',
+    suggestionBase: 'Follows you',
+    id: '5',
+  },
+  {
+    userImg: require('./../../assets/Home-UserSuggestions/6.jpg'),
+    username: 'sharif__alii',
+    suggestionBase: 'popular',
+    id: '6',
+  },
+];
+
+export default userSuggestions;
